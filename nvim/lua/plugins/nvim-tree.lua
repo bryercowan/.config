0b6c0ba4337f838db1ab30b1dab8cb0403e32d95
bryercowan <cowan224@gmail.com>
2025-07-21 14:52:44 -0500

    sync

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,18 +1,21 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    require('nvim-tree').setup({
-      filters = {
-        dotfiles = false,
-        custom = { "^.git$" }, -- Only exclude .git directory, show everything else including .env
-      },
-      git = {
-        ignore = false,
-      },
-      renderer = {
-        highlight_git = true,
-        special_files = { "README.md", "Makefile", "MAKEFILE", ".env" }, -- Highlight .env files
-      },
-    })
-  end
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		require("nvim-tree").setup({
+			filters = {
+				dotfiles = false,
+				custom = { "^.git$" }, -- Only exclude .git directory, show everything else including .env
+			},
+			git = {
+				ignore = false,
+			},
+			renderer = {
+				highlight_git = true,
+				special_files = { "README.md", "Makefile", "MAKEFILE", ".env" }, -- Highlight .env files
+			},
+			view = {
+				width = 45,
+			},
+		})
+	end,
 }
