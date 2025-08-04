fd907cf0a5f63b9aea78dc4757118dc37643e305
bryercowan <cowan224@gmail.com>
2025-08-04 14:10:11 -0500

    updated for warp themes

--- nvim/lua/bryer/init.lua
+++ nvim/lua/bryer/init.lua
@@ -6,24 +6,45 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 vim.o.completeopt = "menu,menuone,noselect"
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx", "*.json", "*.css", "*.scss", "*.html", "*.md", "*.rs", "*.lua", "*.php" },
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end,
+	pattern = {
+		"*.ts",
+		"*.tsx",
+		"*.js",
+		"*.jsx",
+		"*.json",
+		"*.css",
+		"*.scss",
+		"*.html",
+		"*.md",
+		"*.rs",
+		"*.lua",
+		"*.php",
+	},
+	callback = function()
+		vim.lsp.buf.format({ async = false })
+	end,
 })
 
 -- Set indentation for all file types
-vim.opt.expandtab = true      -- Use spaces instead of tabs
-vim.opt.shiftwidth = 2        -- Number of spaces for autoindent
-vim.opt.tabstop = 2           -- Number of spaces for a tab
-vim.opt.softtabstop = 2       -- Number of spaces for Tab key
-vim.opt.smartindent = true    -- Auto-indent new lines
-vim.opt.number = true         -- Show absolute line numbers
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2 -- Number of spaces for autoindent
+vim.opt.tabstop = 2 -- Number of spaces for a tab
+vim.opt.softtabstop = 2 -- Number of spaces for Tab key
+vim.opt.smartindent = true -- Auto-indent new lines
+vim.opt.number = true -- Show absolute line numbers
 vim.opt.relativenumber = true -- Show relative numbers
-vim.opt.number = true         -- Show absolute line numbers
+vim.opt.number = true -- Show absolute line numbers
 vim.opt.relativenumber = true -- Show relative numbers
-vim.opt.wrap = true           -- Enable line wrapping
-vim.opt.linebreak = true      -- Wrap at word boundaries
-vim.opt.showbreak = ">> "     -- Show prefix for wrapped lines
+vim.opt.wrap = true -- Enable line wrapping
+vim.opt.linebreak = true -- Wrap at word boundaries
+vim.opt.showbreak = ">> " -- Show prefix for wrapped lines
--- nvim/lua/plugins/zenburn.lua
+++ nvim/lua/plugins/zenburn.lua
@@ -1,10 +1,10 @@
-return {
-    'phha/zenburn.nvim',
-    config = function()
-        require('zenburn').setup({
-            -- Enable transparency or other settings here if needed
-        })
-        vim.cmd("colorscheme zenburn")
-    end
-}
-
+-- return {
+--     'phha/zenburn.nvim',
+--     config = function()
+--         require('zenburn').setup({
+--             -- Enable transparency or other settings here if needed
+--         })
+--         vim.cmd("colorscheme zenburn")
+--     end
+-- }
+return {}
